750b997e35b4db81ebe7ccf593e9acad4c66bdd2
pradeepsutharr <psuthar318@gmail.com>
2025-06-06 12:43:29 +0530

    delete GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,10 @@ import { useEffect } from "react";
 import "@/styles/globals.css";
 import AOS from "aos";
 import dynamic from "next/dynamic";
-import Script from "next/script";
 
 const Layout = dynamic(() => import("../components/layout/layout"), {
   ssr: false,
 });
-
-const GA_Id = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -17,21 +14,6 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-      {/* Google Analytics */}
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_Id}`}
-        strategy="afterInteractive"
-      />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_Id}');
-        `}
-      </Script>
-
       <Layout>
         <Component {...pageProps} />
       </Layout>
